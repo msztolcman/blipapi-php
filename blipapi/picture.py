fc42c8d320d60055d84ad69aadb32110505dd80f
Marcin Sztolcman <marcin@urzenia.net>
2009-10-04 08:54:30 +0000

    update: podbicie wersji do 0.02.05;

--- blipapi/picture.py
+++ blipapi/picture.py
@@ -3,7 +3,7 @@
 #
 # Blip! (http://blip.pl) communication library.
 # Author: Marcin Sztolcman <marcin /at/ urzenia /dot/ net>
-# Version: 0.02.04
+# Version: 0.02.05
 # Copyright: (r) 2009 Marcin Sztolcman
 # License: http://opensource.org/licenses/gpl-license.php GNU Public License v.2
 
