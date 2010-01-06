d3bc38dab434eb8179fb7d5d5c83f3da4fe3a94e
Marcin Sztolcman <marcin@urzenia.net>
2010-01-06 21:49:40 +0000

    add: przykladowe uzycia blipapi;

---
+++ php/examples/ex1.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Blip! (http://blip.pl) communication library.
+ *
+ * Examples
+ *
+ * @author Marcin Sztolcman <marcin /at/ urzenia /dot/ net>
+ * @version 0.02.30
+ * @version $Id: token_request.php 139 2010-01-06 21:04:29Z urzenia $
+ * @copyright Copyright (c) 2007, Marcin Sztolcman
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License v.2
+ * @package blipapi_examples
+ */
+
+
+require '../lib/blipapi.php';
+
+$blipapi = new BlipApi ();
+
+$b_status = new BlipApi_Status ();
+$b_status->user     = 'mysz';
+$b_status->limit    = 2;
+
+try {
+    $response = $blipapi->read ($b_status);
+}
+catch (RuntimeException $e) {
+    printf ("Blip Error: [%d] %s\n\n%s", $e->getCode (), $e->getMessage (), $e->getTraceAsString ());
+    exit ();
+}
+
+if ($response['status_code'] == 200) {
+    foreach ($response['body'] as $body) {
+        printf ("%s %s\n", $body->created_at, $body->body);
+    }
+}
+else {
+    printf ("Wrong status: [%d] %s\n", $response['status_code'], $response['status_body']);
+}
+
---
+++ php/examples/ex2.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Blip! (http://blip.pl) communication library.
+ *
+ * Examples
+ *
+ * @author Marcin Sztolcman <marcin /at/ urzenia /dot/ net>
+ * @version 0.02.30
+ * @version $Id: token_request.php 139 2010-01-06 21:04:29Z urzenia $
+ * @copyright Copyright (c) 2007, Marcin Sztolcman
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License v.2
+ * @package blipapi_examples
+ */
+
+
+require '../lib/blipapi.php';
+
+$blipapi = new BlipApi ();
+
+$b_bliposphere          = new BlipApi_Bliposphere ();
+$b_bliposphere->limit   = 10;
+$b_bliposphere->include = array ('user');
+
+try {
+    $response = $blipapi->read ($b_bliposphere);
+}
+catch (RuntimeException $e) {
+    printf ("Blip Error: [%d] %s\n\n%s", $e->getCode (), $e->getMessage (), $e->getTraceAsString ());
+    exit ();
+}
+
+if ($response['status_code'] == 200) {
+    foreach ($response['body'] as $body) {
+        printf ("%s [% 20s] %s\n", $body->created_at, $body->user->login , $body->body);
+    }
+}
+else {
+    printf ("Wrong status: [%d] %s\n", $response['status_code'], $response['status_body']);
+}
+
