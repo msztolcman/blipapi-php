da683134310df45bc9e1518c75d6d9a4e6588ca4
Marcin Sztolcman <marcin@urzenia.net>
2010-01-06 23:03:40 +0000

    add: kolejne przyklady, oraz obrazki w nich uzywane; update: nalozenie keyworda ID;

---
+++ examples/ex4.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Blip! (http://blip.pl) communication library.
+ *
+ * Examples
+ *
+ * @author Marcin Sztolcman <marcin /at/ urzenia /dot/ net>
+ * @version 0.02.30
+ * @version $Id$
+ * @copyright Copyright (c) 2007, Marcin Sztolcman
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License v.2
+ * @package blipapi_examples
+ */
+
+
+require '../lib/blipapi.php';
+require '../lib/OAuth.php';
+
+define ('CONSUMER_KEY', 'your app key');
+define ('CONSUMER_SECRET', 'your app secret');
+define ('TOKEN_KEY', 'your token key');
+define ('TOKEN_SECRET', 'your token secret');
+
+$oauth_consumer = new OAuthConsumer (CONSUMER_KEY, CONSUMER_SECRET);
+$oauth_token    = new OAuthToken (TOKEN_KEY, TOKEN_SECRET);
+
+$blipapi = new BlipApi ($oauth_consumer, $oauth_token);
+
+$b_status           = new BlipApi_Status ();
+$b_status->body     = 'Testuję #BlipApiPHP ( http://blipapi.googlecode.com ) - obrazek z demotywatory.pl (r)';
+$b_status->image    = 'image1.jpg';
+
+try {
+    $response = $blipapi->create ($b_status);
+}
+catch (RuntimeException $e) {
+    printf ("Blip Error: [%d] %s\n\n%s", $e->getCode (), $e->getMessage (), $e->getTraceAsString ());
+    exit ();
+}
+
+if ($response['status_code'] == 201) {
+    printf ("Utworzono status: %s\n", $response['headers']['location']);
+}
+else {
+    printf ("Wrong status: [%d] %s\n", $response['status_code'], $response['status_body']);
+}
+
---
+++ examples/ex5.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Blip! (http://blip.pl) communication library.
+ *
+ * Examples
+ *
+ * @author Marcin Sztolcman <marcin /at/ urzenia /dot/ net>
+ * @version 0.02.30
+ * @version $Id$
+ * @copyright Copyright (c) 2007, Marcin Sztolcman
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License v.2
+ * @package blipapi_examples
+ */
+
+
+require '../lib/blipapi.php';
+require '../lib/OAuth.php';
+
+define ('CONSUMER_KEY', 'your app key');
+define ('CONSUMER_SECRET', 'your app secret');
+define ('TOKEN_KEY', 'your token key');
+define ('TOKEN_SECRET', 'your token secret');
+
+$oauth_consumer = new OAuthConsumer (CONSUMER_KEY, CONSUMER_SECRET);
+$oauth_token    = new OAuthToken (TOKEN_KEY, TOKEN_SECRET);
+
+$blipapi = new BlipApi ($oauth_consumer, $oauth_token);
+
+$b_read             = new BlipApi_User ();
+$b_read->include    = array ('current_status', 'avatar', 'pictures');
+
+try {
+    $response = $blipapi->read ($b_read);
+}
+catch (RuntimeException $e) {
+    printf ("Blip Error: [%d] %s\n\n%s", $e->getCode (), $e->getMessage (), $e->getTraceAsString ());
+    exit ();
+}
+
+if ($response['status_code'] == 200) {
+    $body = $response['body'];
+
+    printf ("Login: %s
+Płeć: %s
+Lokalizacja: %s
+Adres awataru: %s
+Ostatni status: %s
+Ostatni status poprzez: %s
+Obrazek ze statusu: %s\n",
+
+        $body->login,
+        $body->sex ? ($body->sex == 'm' ? 'mężczyzna' : 'kobieta') : 'nieznana',
+        $body->location ? $body->location : 'nieznana',
+        $body->avatar ? $body->avatar->url : 'brak',
+        $body->current_status ? $body->current_status->body : 'brak',
+        $body->current_status ? $body->current_status->transport->name : 'brak',
+        $body->current_status && $body->current_status->pictures ? $body->current_status->pictures[0]->url : 'brak'
+    );
+}
+else {
+    printf ("Wrong status: [%d] %s\n", $response['status_code'], $response['status_body']);
+}
+
