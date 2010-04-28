a8fcb2bfd2be1f3537363754bc89ed73330b4f79
Marcin Sztolcman <marcin@urzenia.net>
2010-04-28 15:47:02 +0000

    add: field url_only - ability to return to user full path to avatar, without connecting to blip; add: field size - size of avatar (as above) to return link to (default is standard); add: setters and getters to above fields;

--- lib/blipapi_avatar.php
+++ lib/blipapi_avatar.php
@@ -41,6 +41,30 @@ if (!class_exists ('BlipApi_Avatar')) {
         protected $_image   = '';
 
         /**
+         * If true, return only url to avatar of specified size.
+         *
+         * @accesc protected
+         * @var bool
+         */
+        protected $_url_only    = false;
+
+        /**
+         * Size of avatar to return.
+         * Available values:
+         *  * femto - 15x15 px
+         *  * nano - 30x30 px
+         *  * pico - 50x50 px
+         *  * standard - 90x90 px
+         *  * large - 120x120 px
+         *
+         * Ignored when url_only is set to false.
+         *
+         * @access protected
+         * @var string
+         */
+        protected $_size        = 'standard';
+
+        /**
          * Setter for field: user
          *
          * @param string $value
@@ -61,6 +85,30 @@ if (!class_exists ('BlipApi_Avatar')) {
         }
 
         /**
+         * Setter for field: url_only
+         *
+         * @param bool $value
+         * @access protected
+         */
+        protected function __set_url_only ($value) {
+            $this->_url_only = $value ? true : false;
+        }
+
+        /**
+         * Setter for field: size
+         *
+         * @param bool $value
+         * @access protected
+         */
+        protected function __set_size ($value) {
+            if (!in_array ($value, array ('femto', 'nano', 'pico', 'standard', 'large'))) {
+                throw new InvalidArgumentException ('Unrecognized size of avatar.');
+            }
+
+            $this->_size = $value;
+        }
+
+        /**
          * Get info about users avatar
          *
          * @access public
@@ -70,6 +118,10 @@ if (!class_exists ('BlipApi_Avatar')) {
             if (!$this->_user) {
                 return array ('/avatar', 'get');
             }
+            else if ($this->_url_only) {
+                return array (null, null, null, array ('just_return' => "http://blip.pl/users/$this->_user/avatar/$this->_size.jpg"));
+            }
+
             return array ("/users/$this->_user/avatar", 'get');
         }
 
