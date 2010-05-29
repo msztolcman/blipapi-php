3e6e2453a0c57cf08d75a703255d147c4c350651
Marcin Sztolcman <marcin@urzenia.net>
2010-05-29 23:22:52 +0000

    update: zaktualizowanie wpisow i wersji;

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 #
 # Blip! (http://blip.pl) communication library.
 # Author: Marcin Sztolcman <marcin /at/ urzenia /dot/ net>
-# Version: 0.02.04
+# Version: 0.02.10
 # Copyright: (r) 2009 Marcin Sztolcman
 # License: http://opensource.org/licenses/gpl-license.php GNU Public License v.2
 
@@ -15,7 +15,7 @@ except ImportError:
 
 setup (
     name                = 'BlipApi',
-    version             = '0.02.04',
+    version             = '0.02.10',
     description         = 'Blip! (http://blip.pl) communication library.',
     author              = 'Marcin Sztolcman',
     author_email        = 'marcin@urzenia.net',
@@ -28,10 +28,11 @@ setup (
     classifiers         = [
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords            = 'blip microblogging library',
     license             = 'GPL',
